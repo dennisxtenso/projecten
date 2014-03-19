067b68491f62f7d33350d33cdc9e319cbbc5c6c1
dennisxtenso <dennisxtenso@gmail.com>
2014-03-19 15:31:33 +0100

    Diagram verbeterd
    Meer ruimte tussen de onderdelen

--- crm/index.php
+++ crm/index.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
 
-$tijd = $_POST['tijd'];
-$periode = $_POST['periode'];
-
-//$verticaalbegin = $_POST['verticaalbegin'];
-
-$horizontaal = $_POST['horizontaal'];
-$horizon = count($horizontaal);
-
-$horizontaaleind = $_POST['horizontaaleind'];
-$horizoneind = count($horizontaaleind);
-
-$titels = $_POST['titel'];
-$titel = count($titels);
-
-
-$fases = $_POST['fases'];
-$fase = count($fases);
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$tijd = $_POST['tijd'];
+	$periode = $_POST['periode'];
+	
+	//$verticaalbegin = $_POST['verticaalbegin'];
+	
+	$horizontaal = $_POST['horizontaal'];
+	$horizon = count($horizontaal);
+	
+	$horizontaaleind = $_POST['horizontaaleind'];
+	$horizoneind = count($horizontaaleind);
+	
+	$titels = $_POST['titel'];
+	$titel = count($titels);
+	
+	
+	$fases = $_POST['fases'];
+	$fase = count($fases);
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -30,6 +33,13 @@ html
 {
  font-family: Arial,"Bitstream Vera Sans",Helvetica,Verdana,sans-serif;	
 }
+.inputonderdeel
+{
+	width:200px;
+	padding-left:3px;	
+}
+
+
 #grantt_table
 {
 	width: 800px;
@@ -43,7 +53,6 @@ html
 {
 	text-align:center;
 }
-
 table
 {
 	width: 100%;
@@ -57,6 +66,13 @@ td
 	padding-top:5px;
 	padding-bottom:5px;
 	break-word: word-wrap;
+}
+.tablelinks
+{
+	white-space:nowrap;
+	text-align:right;
+	padding-right:8px;
+	font-weight:bold;
 }
 .vakjein1
 {
@@ -86,41 +102,45 @@ td
     Horizontaal niet voorbij : <input type="text" style="width:100px; padding-left:3px;" name="horizontaaleind" placeholder="horizontaaleind" /><br /><br />    
 -->
 
-<?php
-//TEKSTBALK TOEVOEGEN EN VERWIJDEREN//
-if(!isset($_GET['toevoegen']))
-{
-	$_SESSION['tekst'] = $_SESSION['tekst'];
-}
-else
-{
-	($_SESSION['tekst']);
-	$_SESSION['tekst'] = $_SESSION['tekst']+1;	
-	echo "<p><span style='color:green;'>Er is een tekstbalk toegevoegd</span></p>";
-}
-if(!isset($_GET['verwijderen']))
-{
-	$_SESSION['tekst'] = $_SESSION['tekst'];
-}
-else
-{
-	($_SESSION['tekst']);
-	$_SESSION['tekst'] = $_SESSION['tekst']-1;	
-	echo "<p><span style='color:red;'>Er is een tekstbalk verwijderd</span></p>";
-}
-$tekst = $_SESSION['tekst'];
 
-
-$d = 0;
-$e = 0; //dit is voor de checkboxes//
-while($d <= $tekst)
-{
-	echo '<input type="text" style="width:200px; padding-left:3px;" name="fases[]" value="Onderdeel" /> Horizontaal begin : <input type="text" style="width:100px; padding-left:3px;" name="horizontaal[]" placeholder="Horizontaal" value="0" /> Klaar begin week : <input type="text" style="width:100px; padding-left:3px;" name="horizontaaleind[]" placeholder="horizontaaleind" value="10" /> Titel? <input type="checkbox" name="titel['.$e.']" value="titel"><br />';
-$d++;
-$e++;
-}
-//EINDE TEKSTBALK TOEVOEGEN EN VERWIJDEREN//
-?>
+	<?php
+	//TEKSTBALK TOEVOEGEN EN VERWIJDEREN//
+	if(!isset($_GET['toevoegen']))
+	{
+		$_SESSION['tekst'] = $_SESSION['tekst'];
+	}
+	else
+	{
+		($_SESSION['tekst']);
+		$_SESSION['tekst'] = $_SESSION['tekst']+1;	
+		echo "<p><font color='green'>Er is een tekstbalk toegevoegd</font></p>";
+	}
+	if(!isset($_GET['verwijderen']))
+	{
+		$_SESSION['tekst'] = $_SESSION['tekst'];
+	}
+	else
+	{
+		($_SESSION['tekst']);
+		$_SESSION['tekst'] = $_SESSION['tekst']-1;	
+		echo "<p><font color='red'>Er is een tekstbalk verwijderd</font></p>";
+	}
+	$tekst = $_SESSION['tekst'];
+	
+	
+	$d = 0;
+	$e = 0; //dit is voor de checkboxes//
+	while($d <= $tekst)
+	{
+		echo '<input type="text" class="inputonderdeel" name="fases[]" placeholder="Onderdeel?" value="Onderdeel" /> 
+				Horizontaal begin : <input type="text" class="inputonderdeel" name="horizontaal[]" placeholder="Horizontaal begin?" value="0" /> 
+				Klaar begin week : <input type="text" class="inputonderdeel" name="horizontaaleind[]" placeholder="Klaar begin week?" value="10" /> 
+				Titel? <input type="checkbox" name="titel['.$e.']" value="titel"><br />';
+	$d++;
+	$e++;
+	}
+	//EINDE TEKSTBALK TOEVOEGEN EN VERWIJDEREN//
+	?>
 
 	<a href="http://localhost/projecten/crm/index.php?verwijderen">Tekstveld verwijderen</a><br />
 	<a href="http://localhost/projecten/crm/index.php?toevoegen">Tekstveld toevoegen</a><br /><br />
@@ -137,75 +157,84 @@ $e++;
 </form>
 <br />
 <div id="grantt_table">
-<table cellpadding="1" rules="cols">
-<?php
-
-$a = 0;
-while($a < $fase)
-{
-	echo "<tr>";
-		echo "<td width='auto' style='white-space:nowrap; text-align:right; padding-right:8px; font-weight:bold;'>";
-			echo "$fases[$a]";
-		echo "</td>";
-
-		$c = 0;
-		while($c <= $tijd)
+    <table rules="cols">
+    <?php
+    
+    $a = 0;
+    while($a < $fase)
+    {
+        echo "<tr>";
+            echo "<td class='tablelinks' width='auto'>";
+                echo "$fases[$a]";
+            echo "</td>";
+    
+            $c = 0;
+            while($c <= $tijd)
+            {
+                echo "<td ";
+                $d = $c;
+            //	var_dump($_POST['horizontaal'][$a]);
+                if ($c >= $horizontaal[$a])
+                {
+                    if ($d >= $horizontaaleind[$a])
+                    {
+                        echo "class='vakjein1'>";
+                    }
+                    elseif (!empty($titels[$a]) && $d >= $titels[$a])
+                    {
+                        echo "class='vakjein2'>";
+                    }
+                    else
+                    {
+                        echo "class='vakjein3'>";
+                    }
+                }
+                else
+                {
+                    echo "class='vakjein1'>";
+                }
+                echo "</td>";
+            $c++;
+            }
+        echo "</tr>";
+		
+		echo "<tr style='background-color: transparent;'>";
+		$f = 0;
+		while($f <= $tijd)
 		{
-			echo "<td ";
-			$d = $c;
-		//	var_dump($_POST['horizontaal'][$a]);
-			if ($c >= $horizontaal[$a])
-			{
-				if ($d >= $horizontaaleind[$a])
-				{
-					echo "class='vakjein1'>";
-				}
-				elseif (!empty($titels[$a]) && $d >= $titels[$a])
-				{
-					echo "class='vakjein2'>";
-				}
-				else
-				{
-					echo "class='vakjein3'>";
-				}
-			}
-			else
-			{
-				echo "class='vakjein1'>";
-			}
-			echo "</td>";
-		$c++;
+			echo "<td style='padding-top:0px; padding-bottom:0px; font-size:8px;'>&nbsp;</td>";
+            $f++;
 		}
-	echo "</tr>";
-
-	if ($a == $fase-1)
-	{
-		echo "<td>";
-		echo "</td>";
-	}
-	$b = 0;
-	while($b <= $tijd)
-	{
-		if ($a == $fase-1)
-		{
-			echo "<td>";
-				echo "$b";
-			echo "</td>";
-		}
-	$b++;
-	}
-$a++;
-}	
-?>
-</table>
-<p><?php echo "$periode" ?></p>
+		echo "</tr>";
+		
+        if ($a == $fase-1)
+        {
+            echo "<td>";
+            echo "</td>";
+        }
+        $b = 0;
+        while($b <= $tijd)
+        {
+            if ($a == $fase-1)
+            {
+                echo "<td>";
+                    echo "$b";
+                echo "</td>";
+            }
+        $b++;
+        }
+    $a++;
+    }	
+    ?>
+    </table>
+    <p><?php echo "$periode" ?></p>
 </div>
 <div style="float:right">
-<pre>
-<?php
-print_r ($titels);
-?>
-</pre>
+    <pre>
+		<?php
+			print_r ($titels);
+        ?>
+    </pre>
 </div>
 </body>
 </html>
